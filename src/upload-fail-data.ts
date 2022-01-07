8915b9e3cbda3d7475dc324d35f4c2bbb7ca1808
Max Isom <hi@maxisom.me>
2022-01-07 13:39:06 -0600

    Updates for Summer & Fall 2021

--- src/upload-fail-data.ts
+++ src/upload-fail-data.ts
@@ -1,3 +1,5 @@
+import {transform} from 'stream-transform';
+import {Readable} from 'stream';
 import parse from 'csv-parse';
 import fs from 'fs';
 import {printTable} from 'console-table-printer';
@@ -5,17 +7,16 @@ import prompts from 'prompts';
 import ora from 'ora';
 import {APIClient, IPassFailDrop} from './lib/api';
 
-const BATCH_SIZE = 100;
+const BATCH_SIZE = 10;
 
 interface IParsedRow {
 	course: string;
 	title: string;
-	termcode: string;
-	crn: string;
-	section: string;
+	total: number;
 	failed: number;
 	dropped: number;
-	total: number;
+	year: number;
+	semester: string;
 }
 
 const SEMESTER_MAPPING: Record<string, IPassFailDrop['semester']> = {
@@ -29,7 +30,7 @@ const SEMESTER_MONTH_MAPPING: Record<string, IPassFailDrop['semester']> = {
 	'01': 'SPRING'
 };
 
-const getNRecords = async (parser: parse.Parser, n: number): Promise<IParsedRow[]> => {
+const getNRecords = async (parser: Readable, n: number): Promise<IPassFailDrop[]> => {
 	const result: any[] = [];
 
 	return new Promise<any>((resolve, reject) => {
@@ -44,6 +45,8 @@ const getNRecords = async (parser: parse.Parser, n: number): Promise<IParsedRow[
 	});
 };
 
+const COURSE_TO_SECTION_COUNTER = new Map<string, number>();
+
 const reshapeRecord = (record: IParsedRow): IPassFailDrop => {
 	const subject = record.course.match(/([A-Z])+/g);
 	const crse = record.course.match(/(\d)+/g);
@@ -52,15 +55,18 @@ const reshapeRecord = (record: IParsedRow): IPassFailDrop => {
 		throw new Error(`Could not parse ${JSON.stringify(record)}`);
 	}
 
-	const year = Number.parseInt(record.termcode.slice(0, 4), 10);
-	const semester = record.termcode.slice(4);
+	const key = `${subject[0]}${crse[0]}`;
+
+	COURSE_TO_SECTION_COUNTER.set(key, (COURSE_TO_SECTION_COUNTER.get(key) ?? 0) + 1);
+
+	const section = (COURSE_TO_SECTION_COUNTER.get(key) ?? 1).toString().padStart(2, '0');
 
 	return {
 		courseSubject: subject[0],
 		courseCrse: crse[0],
-		year,
-		semester: SEMESTER_MONTH_MAPPING[semester],
-		section: record.section,
+		year: record.year,
+		semester: SEMESTER_MAPPING[record.semester],
+		section,
 		failed: record.failed,
 		dropped: record.dropped,
 		total: record.total
@@ -75,32 +81,40 @@ const reshapeRecord = (record: IParsedRow): IPassFailDrop => {
 		process.exit(1);
 	}
 
-	const parser = fs.createReadStream(path).pipe(parse({
-		columns: [
-			'course',
-			'title',
-			'termcode',
-			'section',
-			'crn',
-			'total',
-			'failed',
-			'dropped'
-		],
-		from: 1,
-		cast: (value, context) => {
-			if (['total', 'failed', 'dropped'].includes(context.column as string)) {
-				if (value.trim() === '') {
-					return 0;
+	const parser = fs.createReadStream(path)
+		.pipe(parse({
+			columns: [
+				'course',
+				'title',
+				'total',
+				'failed',
+				'dropped',
+				'year',
+				'semester'
+			],
+			from: 2,
+			cast: (value, context) => {
+				if (['total', 'failed', 'dropped', 'year'].includes(context.column as string)) {
+					if (value.trim() === '') {
+						return 0;
+					}
+
+					return Number.parseInt(value, 10);
 				}
 
-				return Number.parseInt(value, 10);
+				return value;
+			}
+		}))
+		.pipe(transform((record: IParsedRow, callback) => {
+			if (Number.isNaN(record.failed)) {
+				callback(null, null);
+				return;
 			}
 
-			return value;
-		}
-	}));
+			callback(null, reshapeRecord(record));
+		}));
 
-	let records = (await getNRecords(parser, 5)).map(record => reshapeRecord(record));
+	let records = (await getNRecords(parser, 10));
 
 	console.log('This is what the data looks like so far:');
 	printTable(records);
@@ -135,18 +149,26 @@ const reshapeRecord = (record: IParsedRow): IPassFailDrop => {
 
 	// Upload in batches
 	for await (const record of parser) {
-		if (records.length >= BATCH_SIZE) {
-			await client.putManyPassFailDrop(records);
-			records = [];
-		}
+		records.push(record);
 
-		// Omit empty rows
-		if (record.course !== '') {
-			records.push(reshapeRecord(record));
+		if (records.length >= BATCH_SIZE) {
+			try {
+				await client.putManyPassFailDrop(records);
+			} catch (error: unknown) {
+				console.error(records);
+				throw error;
+			}
+
+			records = [];
 		}
 	}
 
-	await client.putManyPassFailDrop(records);
+	try {
+		await client.putManyPassFailDrop(records);
+	} catch (error: unknown) {
+		console.error(records);
+		throw error;
+	}
 
 	spinner.succeed('Finished uploading data.');
 })();
